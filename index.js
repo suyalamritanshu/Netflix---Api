6926ce437123a29c76641471be52b9f33e13a671
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-04-17 16:47:07 +0530

    fixed cors policy

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ dotenv.config();
 //Database Connection
 
 DbConnect();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+  })
 
 app.use(express.json());
 
